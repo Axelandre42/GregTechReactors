c208d624032d14354543e38571cca5721d5cea5d
Axelandre42 <axelandre42@yahoo.fr>
2014-11-10 19:02:19 +0100

    Setting up eclipse and Creating modfile.

---
+++ src/main/java/net/axelandre42/gregtechreactors/GregTechReactors.java
@@ -0,0 +1,18 @@
+package net.axelandre42.gregtechreactors;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = GregTechReactors.MODID, version = GregTechReactors.VERSION)
+public class GregTechReactors {
+	// ---------- [Info fields] ----------
+	public static final String MODID = "GregTechReactors";
+	public static final String VERSION = "0.01";
+	
+	// ---------- [Loading] ----------
+	@EventHandler
+	public void load(FMLInitializationEvent e) {
+		
+	}
+}
